GrapheneOS/platform_packages_providers_ContactsProvider

cscopes: add helper methods for the Contact Scopes UI
Change-Id: Id5214328ee096e2372cda3d14d3e2be1c51fe796

src/com/android/providers/contacts/ScopedContactsProvider.java
@@ -456,4 +456,36 @@ public class ScopedContactsProvider extends RedirectedContentProvider {
             dst.addAll(arr);
         }
     }
+
+    private void checkContactScopesUiCaller() {
+        // UI is implemented in PermissionController
+        requireContext().enforceCallingPermission(Manifest.permission.GRANT_RUNTIME_PERMISSIONS, null);
+    }
+
+    @Nullable
+    @Override
+    public Bundle call(String method, @Nullable String arg, @Nullable Bundle extras) {
+        Bundle superRes = super.call(method, arg, extras);
+
+        switch (method) {
+            case ContactScopesApi.METHOD_GET_ID_FROM_URI: {
+                checkContactScopesUiCaller();
+                Uri uri = extras.getParcelable(ContactScopesApi.KEY_URI, Uri.class);
+                return Binder.withCleanCallingIdentity(() ->
+                        ContactScopesUiHelper.getIdFromUri(this, uri));
+            }
+            case ContactScopesApi.METHOD_GET_VIEW_MODEL: {
+                checkContactScopesUiCaller();
+                return Binder.withCleanCallingIdentity(() ->
+                        ContactScopesUiHelper.getAppScopesViewModel(this, arg));
+            }
+            case ContactScopesApi.METHOD_GET_GROUPS: {
+                checkContactScopesUiCaller();
+                return Binder.withCleanCallingIdentity(() ->
+                        ContactScopesUiHelper.getGroups(this));
+            }
+        }
+
+        return superRes;
+    }
 }

VoltageOS/packages_providers_ContactsProvider

Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

src/com/android/providers/contacts/ScopedContactsProvider.java
@@ -486,4 +486,36 @@ public class ScopedContactsProvider extends RedirectedContentProvider {
             dst.addAll(arr);
         }
     }
+
+    private void checkContactScopesUiCaller() {
+        // UI is implemented in PermissionController
+        requireContext().enforceCallingPermission(Manifest.permission.GRANT_RUNTIME_PERMISSIONS, null);
+    }
+
+    @Nullable
+    @Override
+    public Bundle call(String method, @Nullable String arg, @Nullable Bundle extras) {
+        Bundle superRes = super.call(method, arg, extras);
+
+        switch (method) {
+            case ContactScopesApi.METHOD_GET_IDS_FROM_URIS: {
+                checkContactScopesUiCaller();
+                Uri[] uris = extras.getParcelableArray(ContactScopesApi.KEY_URIS, Uri.class);
+                return Binder.withCleanCallingIdentity(() ->
+                        ContactScopesUiHelper.getIdsFromUris(this, uris));
+            }
+            case ContactScopesApi.METHOD_GET_VIEW_MODEL: {
+                checkContactScopesUiCaller();
+                return Binder.withCleanCallingIdentity(() ->
+                        ContactScopesUiHelper.getAppScopesViewModel(this, arg));
+            }
+            case ContactScopesApi.METHOD_GET_GROUPS: {
+                checkContactScopesUiCaller();
+                return Binder.withCleanCallingIdentity(() ->
+                        ContactScopesUiHelper.getGroups(this));
+            }
+        }
+
+        return superRes;
+    }
 }